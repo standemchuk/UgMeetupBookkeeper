23d9970e2e338457eb37685c091e90f684495ada
Stanislav Demchuk <standemchuk@gmail.com>
2018-02-17 12:43:56 +0200

    added keyboard spacer, fixed issue with the status bar

--- App.js
+++ App.js
@@ -4,19 +4,24 @@
  * @flow
  */
 
-import React  from 'react';
+import React from 'react';
+import { View, StatusBar, Platform } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
 import HomeScreen from './src/screens/HomeScreen';
 import AddExpenseScreen from './src/screens/AddExpenseScreen';
 import ExpensesInfoScreen from './src/screens/ExpensesInfoScreen';
 
-export default TabNavigator({
+const Navigation = TabNavigator({
     Home: { screen: HomeScreen },
     AddExpense: { screen: AddExpenseScreen },
     ExpensesInfo: { screen: ExpensesInfoScreen }
 }, {
     navigationOptions: ({ navigation }) => ({
+        headerStyle: {
+            marginTop: 40
+        },
         tabBarIcon: ({ focused, tintColor }) => {
             const { routeName } = navigation.state;
             let iconName;
@@ -40,8 +45,25 @@ export default TabNavigator({
     }),
     tabBarOptions: {
         activeTintColor: 'tomato',
-        inactiveTintColor: 'gray',
+        inactiveTintColor: 'gray'
     },
     animationEnabled: false,
     swipeEnabled: false,
-})
+});
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <View style={{ height: Platform.OS === 'ios' ? 20 : StatusBar.currentHeight }}>
+                    <StatusBar
+                        translucent
+                        backgroundColor={'transparent'}
+                    />
+                </View>
+                <Navigation />
+                <KeyboardSpacer />
+            </View>
+        )
+    }
+}
